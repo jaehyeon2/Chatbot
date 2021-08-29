9ff5786452759d610b625fb524937835439fa4b1
강재현 <wol5971@gmail.com>
2021-08-29 10:10:40 +0000

    commit

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-spp.use(sessionMiddleware);
+app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
