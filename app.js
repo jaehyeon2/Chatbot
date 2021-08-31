95e4847097f820bb17e9dc86ce8b9d6920f59784
강재현 <wol5971@gmail.com>
2021-08-31 10:08:34 +0000

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const path=require('path');
-const morgan=require('morgan')
+const morgan=require('morgan');
 const cookieParser=require('cookie-parser');
 const session=require('express-session');
 const nunjucks=require('nunjucks');
@@ -8,7 +8,7 @@ const dotenv=require('dotenv');
 const passport=require('passport');
 
 dotenv.config();
-
+const webSocket=require('./socket');
 const indexRouter=require('./routes');
 const authRouter=require('./routes/auth');
 const connect=require('./schemas');
@@ -44,8 +44,15 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+
+app.use((req, res, next)=>{
+	const error=new Error(`${req.method} ${req.url} 라우터가 존재하지 않습니다.`);
+	error.status=404;
+	next(error);
+});
 
 app.use((err, req, res, next)=>{
 	res.locals.message=err.message;
@@ -54,6 +61,8 @@ app.use((err, req, res, next)=>{
 	res.render('error');
 });
 
-app.listen(app.get('port'), ()=>{
+const server=app.listen(app.get('port'), ()=>{
 	console.log(app.get('port'), 'port waiting');
 });
+
+webSocket(server, app, sessionMiddleware);
